a271100227c96b69477bae3a71149c6ebbfabedc
David Rupp <david@ruppworks.com>
2013-11-18 14:29:53 -0500

    first cut at books function

--- src/bookworm/core.clj
+++ src/bookworm/core.clj
@@ -1,6 +1,11 @@
 (ns bookworm.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn books [& fltr]
+  (let [book-dir "/Users/Thoughtworker/Dropbox/Books"]
+    (->> book-dir
+         clojure.java.io/file
+         file-seq
+         (filter #(.isFile %))
+         (filter #(re-find #"^.*\.(pdf|PDF)$" (.getName %)))
+         (map (fn [file] [(.getPath file) (.getName file)]))
+         (into {}))))
